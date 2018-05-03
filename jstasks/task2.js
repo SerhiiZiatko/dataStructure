3abd3a0f67e81b772dd9d8e1a6c00fafda4da37a
litvinovvl <lvb.kam@gmail.com>
2018-05-03 10:08:32 +0300

    litvinovvl. solved all tasks, updated task2.js

--- jstasks/task2.js
+++ jstasks/task2.js
@@ -38,3 +38,25 @@ function makeW2(fontWidth, height, length) {
 }
 
 //**------------------------------------------------------------------------------------------------------------------//
+
+function makeW3(fontWidth, height, length) {
+    length = length || 2;
+    let tmp = [];
+    for (let i = 0; i < height; i++) {
+        tmp[i] = (' '.repeat(i) + '*'.repeat(fontWidth) + ' '.repeat(height-1-i));
+    }
+    for (let i = 0; i < height; i++) {
+        tmp[i] = tmp[i].slice(0, tmp[i].length -1);
+    }
+    for (let i = 0, j = height-1; i < height, j >= 0; i++, j--) {
+        tmp[i] = tmp[i] + tmp[j];
+    }
+    let len = tmp[0].length;
+    for (let i = 0; i < height; i++) {
+        tmp[i] = tmp[i].slice(0, len);
+    }
+    for (let i = 0; i < height; i++) {
+        tmp[i] = tmp[i].repeat(length);
+    }
+    return tmp;
+}
